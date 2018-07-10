3626baeeb5e1115ee7479a916754e4014b0adecd
Huong Dang <huongd97@knights.ucf.edu>
2018-07-09 23:48:42 -0400

    feat(components): Add album list and album detail components

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -2,7 +2,26 @@
 import React from 'react';
 import { View, Text } from 'react-native';
 
-// Class component
+// Functional component
+const AlbumDetail = (props) => {
+    // Each album contains these properties:
+    // title
+    // artist
+    // url
+    // image
+    // thumbnail_image
+    
+    return (
+        <View>
+            <Text>{props.album.title}</Text>
+            <Text>{props.album.artist}</Text>
+        </View>
+    );
+};
+
+const styles = {
+
+}
 
 // Expose the component
 export default AlbumDetail;
--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -3,16 +3,43 @@
 // and rendering a list of cards with each card showing
 // details for an album
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
-import Axios from 'axios'; // Responsible for get requests
+import { View } from 'react-native';
+import axios from 'axios'; // Responsible for get requests
+import AlbumDetail from './AlbumDetail';
 
 // Create the class based components
+
+// Lifecycle methods - class based compoents advantage over functional components
+// Know exactly when it will be rendered to the screen using lifecycle methods
 class AlbumList extends Component {
+    // Set initial empty
+    state = {albums: []};
+
+    // Any time the component is about to be
+    // rendered to the screen, this method will be
+    // automatically executed
+    componentWillMount() {
+        // Get all albums from Heroku
+        axios.get('https://rallycoding.herokuapp.com/api/music_albums')
+            .then((response) => {
+                // setState is defined in Component. setState
+                // updates our component's state and it notifies that
+                // component's state has changed and to rerender content
+                this.setState({albums: response.data});
+                console.log(this.state);
+            });
+    };
+
+    renderAlbums() {
+        return this.state.albums.map(album =>
+            <AlbumDetail key={album.title} album={album} />
+        );
+    }
 
     render() {
         return (
             <View>
-                <Text>Album List!!!</Text>
+                {this.renderAlbums()}
             </View>
         );
     };
